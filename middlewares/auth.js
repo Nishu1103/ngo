84cb3255849bbae51c5d47b427670dcabdb041e8
Nishu1103 <128515332+Nishu1103@users.noreply.github.com>
2024-08-30 11:24:40 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,26 +5,32 @@ import { catchASyncError } from './catchASyncError.js';
 
 export const isAuthorized = catchASyncError(async (req, res, next) => {
     const authHeader = req.get('Authorization');
-    if (!authHeader) {
+
+    // Check for the 'Authorization' header
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return next(new ErrorHandler('Not authenticated', 401));
     }
+
     const token = authHeader.split(' ')[1];
     let decodedToken;
+
     try {
         decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
     } catch (err) {
-        err.statusCode = 500;
-        return next(new ErrorHandler('Token is not valid', 500));
+        return next(new ErrorHandler('Token is not valid', 401));
     }
 
-    if (!decodedToken) {
+    // Check if the token is decoded properly
+    if (!decodedToken || !decodedToken.id) {
         return next(new ErrorHandler('Not authenticated', 401));
     }
 
-    req.user = await User.findById(decodedToken.id);
-    if (!req.user) {
+    // Find the user by ID from the decoded token
+    const user = await User.findById(decodedToken.id);
+    if (!user) {
         return next(new ErrorHandler('User not found', 404));
     }
 
+    req.user = user; // Attach the user object to the request
     next();
 });
